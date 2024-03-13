13a5b18244f856a868cbbf6aee6d969cfbdfa603
FRJ74 <frankr74@gmail.com>
2024-03-13 15:52:32 -0600

    Sort songs alphbetically with sort()method

--- script.js
+++ script.js
@@ -117,4 +117,14 @@ let userData = {
   
 
   renderSongs(userData?.songs);
+
+  const sortSongs = () => {
+    userData?.songs.sort((a,b) => {
+      if (a.title < b.title) {
+        return -1;
+         }
+    });
+  };
+
+
   
